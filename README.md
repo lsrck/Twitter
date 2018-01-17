803ce59d02b016c941006629a28d44831f32182c
Louis Aurick <louisaurick@ntymail.com>
2018-01-17 18:46:17 +0100

    last update + readme

---
+++ README.md
@@ -0,0 +1,7 @@
+Fonctionnalités du bot:
+
+.poster un message sur ma timeline
+.poster un message en taggant une personne
+.créer un stream qui affiche tous les tweets qui mentionnent tea ou coffee
+.poster un message sur la timeline de plusieurs journalistes
+.liker les 20 derniers tweets d'une personne
--- twitter.rb
+++ twitter.rb
@@ -2,12 +2,35 @@ require 'twitter'
 require "dotenv"
 Dotenv.load
 
-client = Twitter::REST::Client.new do |config|
+$client = Twitter::REST::Client.new do |config|
   config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
   config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
   config.access_token        = ENV["ACCESS_TOKEN"]
   config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
-p client
+p $client
 end
 
-client.update("Thx for your help w/ dotenv @Mamarieponpont. Btw I love flowers")
+$client.update("Bonjour monde!")
+
+def twitter_des_journalistes(*liste)
+	liste.each {|journaliste| $client.update("#{journaliste} Je suis élève à The Hacking Projet une formation gratuite au code")}
+end
+twitter_des_journalistes("@thpnico", "@Fabien_971‏", "@Mamarieponpont")
+
+def like_tweets(journaliste)
+	tweets_id = $client.user_timeline(journaliste)
+	tweets_id.each {|id| $client.favorite(id)}
+end
+like_tweets("@Mamarieponpont")
+
+clientstream = Twitter::Streaming::Client.new do |config|
+  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+  config.access_token        = ENV["ACCESS_TOKEN"]
+  config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+end
+
+topics = ["coffee","tea"]
+clientstream.filter(track: topics.join(",")) do |object|
+  puts object.text if object.is_a?(Twitter::Tweet)
+end
